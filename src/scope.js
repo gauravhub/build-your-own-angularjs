eb78c74298f2d0c7c5b056e1125417e967543b10
Gaurav Dhamija <gaurav.dhamija@software-innovation.com>
2015-02-06 13:33:46 -0500

    Add support for applyasync and postDigest operations

--- src/scope.js
+++ src/scope.js
@@ -9,17 +9,70 @@ function Scope() {
     this.$$watches = [];
     this.$$lastDirtyWatch = null;
     this.$$evalAsyncQueue = [];
+    this.$$applyAsyncQueue = [];
+    this.$$phase = null;
+    this.$$applyAsyncId = null;
+    this.$$postDigestQueue = [];
+}
+
+Scope.prototype.$$postDigest = function(fn) {
+    this.$$postDigestQueue.push(fn);
+}
+
+Scope.prototype.$beginPhase = function(phase) {
+    if (this.$$phase) {
+        throw this.$$phase + ' already in progress.';
+    }
+    this.$$phase = phase;
+}
+
+Scope.prototype.$clearPhase = function() {
+    this.$$phase = null;
+}
+
+Scope.prototype.$applyAsync = function(expr) {
+    var self = this;
+    self.$$applyAsyncQueue.push(
+        function() {
+            self.$eval(expr);
+    });
+
+    if (self.$$applyAsyncId === null) {
+        self.$$applyAsyncId = setTimeout(
+            function() {
+                self.$apply(_.bind(self.$$flushApplyAsync, self));
+            }, 0);
+    }
+}
+
+Scope.prototype.$$flushApplyAsync = function() {
+    while (this.$$applyAsyncQueue.length) {
+        this.$$applyAsyncQueue.shift()();
+    }
+    this.$$applyAsyncId = null;
 }
 
 Scope.prototype.$evalAsync = function(expr) {
+    var self = this;
+    if (!self.$$phase && !self.$$evalAsyncQueue.length) {
+        setTimeout(
+            function() {
+                if (self.$$evalAsyncQueue.length) {
+                    self.$digest();
+                }
+            }, 
+        0);
+    }
     this.$$evalAsyncQueue.push({scope: this, expression: expr});
-};
+}
 
 Scope.prototype.$apply = function(expr) {
     try {
+        this.$beginPhase("$apply");
         return this.$eval(expr);
     } 
     finally {
+        this.$clearPhase();
         this.$digest();
     }
 }
@@ -80,6 +133,13 @@ Scope.prototype.$$digestOnce = function() {
 Scope.prototype.$digest = function() {
     var dirty, ttl = 10;
     this.$$lastDirtyWatch = null;
+    this.$beginPhase("$digest");
+
+    if (this.$$applyAsyncId) {
+        clearTimeout(this.$$applyAsyncId);
+        this.$$flushApplyAsync();
+    }
+
     do {
         while (this.$$evalAsyncQueue.length) {
             var asyncTask = this.$$evalAsyncQueue.shift();
@@ -89,8 +149,14 @@ Scope.prototype.$digest = function() {
         dirty = this.$$digestOnce();
         ttl = ttl - 1;
         if((dirty || this.$$evalAsyncQueue.length) && !ttl) {
+            this.$clearPhase();
             throw "10 digest iterations reached";
         }
     }
     while (dirty || this.$$evalAsyncQueue.length);
+    this.$clearPhase();
+
+    while (this.$$postDigestQueue.length) {
+        this.$$postDigestQueue.shift()();
+    }
 }
